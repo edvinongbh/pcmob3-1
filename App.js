ab9045056a9d1487d1e60f6df4af3062ed64fe52
ongboonhan <edvinongbh@gmail.com>
2022-10-29 13:19:10 +0800

    half way

--- App.js
+++ App.js
@@ -1,41 +1,81 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import BlockRGB from "./components/BlockRGB";
 
+function HomeScreen({ navigation }) {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+  ]);
 
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity onPress={() => navigation.navigate("Details")}>
+        <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+      </TouchableOpacity>
+    );
+  }
 
-function HomeScreen(){
+  function addColor() {
+    setColorArray([
+      {
+        red: Math.floor(Math.random() * 255),
+        green: Math.floor(Math.random() * 255),
+        blue: Math.floor(Math.random() * 255),
+        id: colorArray.length.toString(),
+      },
+      ...colorArray,
+    ]);
+  }
 
-return (<View style={styles.container}>
-  <Text>Open up App.js to start working on your !</Text>
-  <StatusBar style="auto" />
-</View>
-);
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={addColor}
+        style={{ height: 40, justifyContent: "center" }}
+      >
+        <Text>Add Color</Text>
+      </TouchableOpacity>
 
+      <FlatList
+        data={colorArray}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+      />
+    </View>
+  );
 }
 
-   const Stack = createStackNavigator()
+function DetailsScreen() {
+  return <Text>Details Screen</Text>;
+}
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-  
-<NavigationContainer>
-<Stack.Navigator> 
-       <Stack.Screen name='Home' component={HomeScreen}/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
-</NavigationContainer>
-
-
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/BlockRGB.js
+++ components/BlockRGB.js
@@ -1,23 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function BlockRGB(props) {
-    const red = props.red;
-    const green = props.green;
-    const blue = props.blue;
-
   return (
     <View
-    style={ 
-        { 
-            backgroundColor: `rgb( ${red}, ${green}, ${blue} )`,
-            padding: 30,
-            width: "100%"
-
-    }}
-     > </View>
+      style={{
+        backgroundColor: `rgb( ${props.red}, ${props.green}, ${props.blue})`,
+        padding: 30,
+        width: "100%",
+      }}
+    ></View>
   );
 }
-
-
-
